692a1d95b1e46121622fc4085201e4a5c6b993f3
kryptoslex <kryptoslex@gmail.com>
2017-09-26 17:15:16 +0800

    added sound

--- Retro Calc/ViewController.swift
+++ Retro Calc/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var btnSounds: AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
+        let path: String? = Bundle.main.path(forResource: "btn", ofType: "wav")
+        if let btnPath = path {
+            let urlPath: URL? = URL.init(fileURLWithPath: btnPath)
+            print("urlpath = \(urlPath!)")
+            
+            do{
+                try btnSounds = AVAudioPlayer.init(contentsOf: urlPath!)
+                btnSounds.prepareToPlay()
+            }catch let err as NSError{
+                print("error playing = \(err.debugDescription)")
+            }
+        }
+        
     }
 
+    
+    @IBAction func numberPressed(sender:UIButton){
+        playSound()
+    }
+    
+    func playSound(){
+        if btnSounds.isPlaying {
+            btnSounds.stop()
+        }
+        btnSounds.play()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
